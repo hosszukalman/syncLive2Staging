0ce9bcea7c83203d37e84e4e78ec71373b556b10
Kalman Hosszu <hosszu.kalman@gmail.com>
2012-02-07 19:26:57 +0100

    Clear staging db: drop all tables.

--- syncLive2Staging.sh
+++ syncLive2Staging.sh
@@ -20,3 +20,8 @@ type drush &>/dev/null && (echo "Clear Drupal cache tables..."; drush cc all;)
 echo "Export staging database...";
 mysqldump -h$stagingHost -u$stagingUser -p$stagingPass $stagingDB --add-drop-table -e | gzip > $backupPath`date "+%y%m%d_%H%M"`_$stagingDB.sql.gz;
 echo "Staging database is exported to $backupPath`date "+%y%m%d_%H%M"`_$stagingDB.sql.gz";
+
+# Remove all tables from staging DB.
+echo "Drop all staging tables...";
+mysqldump -h$stagingHost -u$stagingUser -p$stagingPass --add-drop-table --no-data $stagingDB | grep ^DROP | mysql -h$stagingHost -u$stagingUser -p$stagingPass $stagingDB;
+echo "All staging tables dropped!";
