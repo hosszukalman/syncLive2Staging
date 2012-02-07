00859dbef9feb2aa85c14552d925c5690b0362d9
Kalman Hosszu <hosszu.kalman@gmail.com>
2012-02-07 19:25:14 +0100

    Create variables

---
+++ syncLive2Staging.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Staging site's data.
+stagingUser=""
+stagingPass=""
+stagingDB=""
+stagingHost="localhost";
+backupPath="./"
+
+# Live site's data.
+liveUser="";
+livePass="";
+liveDB="";
+liveHost="localhost";
