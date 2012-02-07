9546b54604bd72b0afd070bd1761fe140b95cd47
Kalman Hosszu <hosszu.kalman@gmail.com>
2012-02-07 19:26:23 +0100

    Create backup from staging site.

--- syncLive2Staging.sh
+++ syncLive2Staging.sh
@@ -15,3 +15,8 @@ liveHost="localhost";
 
 # Clear cache tables before backup.
 type drush &>/dev/null && (echo "Clear Drupal cache tables..."; drush cc all;)
+
+# Create backup from staging DB.
+echo "Export staging database...";
+mysqldump -h$stagingHost -u$stagingUser -p$stagingPass $stagingDB --add-drop-table -e | gzip > $backupPath`date "+%y%m%d_%H%M"`_$stagingDB.sql.gz;
+echo "Staging database is exported to $backupPath`date "+%y%m%d_%H%M"`_$stagingDB.sql.gz";
